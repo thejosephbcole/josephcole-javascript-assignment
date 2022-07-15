a0d958631385c3da1b92f2d6205f26dbebd752e3
Joseph B. Cole <105296063+thejosephbcole@users.noreply.github.com>
2022-07-15 20:32:39 +0000

    attempted test-challenge-1

--- src/assignment.js
+++ src/assignment.js
@@ -14,10 +14,16 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.sumOfNumbers = sumOfNumbers;
+  let total = 0;
 
+  for (let i = 0; i < arrayOfNumbers.length; i += 1) 
+    {
+      total += arrayOfNumbers[i];
+    }
+  return total;
+}
+
+assignment.sumOfNumbers = sumOfNumbers;
 /**
  * Challenge - 2
  * 
@@ -28,9 +34,15 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let count = 0;
+
+  for (let i = 0; i <= arrayOfNumbers.length; i++) 
+  {
+    if (arrayOfNumbers[i] % 2 === 0) {count++};
+  }
+  return count
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
